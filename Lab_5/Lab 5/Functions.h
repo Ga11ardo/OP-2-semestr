444f270bae7d987cebbfdb8550031d01fb136fdb
Ga11ardo <90717318+Ga11ardo@users.noreply.github.com>
2023-05-26 09:12:11 +0300

    Add files via upload

---
+++ Lab_5/Lab 5/Functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Classes.h"
+
+void input(int& val);
+TIntNumber* InputBinNumbers(int);
+TIntNumber* InputOctNumbers(int);
+void PrintArrayAll(TIntNumber*, TIntNumber*, int, int);
+void PrintArray(TIntNumber*, int);
+int FindTheSmallestDecNumber(TIntNumber*, TIntNumber*, int, int);
+void Operations(TIntNumber*, TIntNumber*, int, int);
